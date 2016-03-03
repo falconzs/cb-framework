220b018b84f43b483051bdc3745434f22dfe1790
falconzs <falconfiftyfive@yahoo.co.uk>
2016-03-03 21:16:34 +0000

    Add index.js with all components and update package.json to point to index as main.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+exports.app = require('./app');
+exports.bot = require('./bot');
+exports.cbdummy = require('./cb-dummy');
+exports.cbjsdummy = require('./cbjs-dummy');
+exports.collection = require('./collection');
+exports.library = require('./library');
+exports.message = require('./message');
+exports.user = require('./user');
+
+exports.pluginAbstract = require('./plugins/abstract');
+exports.pluginHelp = require('./plugins/abstract');
