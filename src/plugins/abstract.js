5568103926cc62408a2fb58f43f406bcd76fbe1c
falconzs <falconfiftyfive@yahoo.co.uk>
2016-04-27 12:04:49 +0100

    Convert plugin abstract and help to ES6 class syntax.

--- src/plugins/abstract.js
+++ src/plugins/abstract.js
@@ -1,27 +1,26 @@
+"use strict";
 
-var DefineClass = require('define-class'),
-    abstract = DefineClass({
-        init: function() {
-            this.settingsMap = null;
-        },
+class Abstract {
+    constructor() {
+        this.settingsMap = null;
+    }
 
-        setDependencies: function(api, library) {
-            this.api = api;
-            this.library = library;
-        },
+    setDependencies(api) {
+        this.api = api;
+    }
 
-        satisfies: function (command) {
-            if (!this.commands) {
-                return false;
-            }
-            var index, request;
-            for (index in this.commands) {
-                if (index == command) {
-                    return this.commands[index];
-                }
-            }
+    satisfies(command) {
+        if (!this.commands) {
             return false;
         }
-    }, {});
+        var index, request;
+        for (index in this.commands) {
+            if (index == command) {
+                return this.commands[index];
+            }
+        }
+        return false;
+    }
+}
 
-module.exports = abstract;
+module.exports = Abstract;
