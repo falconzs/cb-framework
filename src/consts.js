aedf026ba176fec9a4b07b74573464c5e1e8c08d
falconzs <falconfiftyfive@yahoo.co.uk>
2016-04-27 10:41:18 +0100

    Split library into constants and prototypes.

---
+++ src/consts.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+    // special characters
+    chars: {
+        black_arrow_right: '\u25ba'
+    },
+
+    // color mappings
+    colors: {
+        black: '#000',
+        white: '#fff',
+        grey: '#ddd',
+
+        light_red: '#FF4343',
+        red: '#FF1010',
+        dark_red: '#DC0000',
+
+        light_orange: '#FF7C2A',
+        orange: '#F65F00',
+        dark_orange: '#C34B00',
+
+        light_yellow: '#FFFF266',
+        yellow: '#FE3',
+        dark_yellow: '#E6D200',
+
+        light_green: '00E600',
+        green: '#00B300',
+        dark_green: '#008000',
+
+        light_blue: '#1151FE',
+        blue: '#013CDB',
+        dark_blue: '#012EA8',
+
+        light_purple: '#AA00FF',
+        purple: '#8800CC',
+        dark_purple: '#660099',
+
+        light_pink: '#FF4DF3',
+        pink: '#FF00EE',
+        dark_pink: '#CC00BE'
+    }
+};
---
+++ src/prototypes.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var hexColor    = /^#?(?:[0-9a-f]{3}){1,2}$/i,
+    titleCase   = /\w\S*/g,
+    regexEscape = /[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g;
+
+/**
+ * Capitalize the first letter of the string.
+ * @returns {string}
+ */
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.substr(1).toLowerCase();
+};
+
+/**
+ * Capitalizes the first letter of each word.
+ * @returns {string}
+ */
+String.prototype.toTitleCase = function() {
+    return this.replace(titleCase, function (word) {
+        return word.capitalize();
+    });
+};
+
+/**
+ * Compares the two strings for equality.
+ * @param {string} compare
+ * @param {boolean} caseSensitive Optional, defaults to false.
+ * @returns {boolean}
+ */
+String.prototype.isEqual = function(compare, caseSensitive) {
+    var compare = compare + '',
+        caseSensitive = caseSensitive || false;
+    if (!!caseSensitive) {
+        return this === compare;
+    } else {
+        return this.toLowerCase() === compare.toLowerCase();
+    }
+};
+
+/**
+ * Determines if the string is a hexadecimal color. '#' is optional, works with 3 or 6
+ * charater length colors.
+ * @returns {boolean}
+ */
+String.prototype.isHexColor = function() {
+    return hexColor.test(this.trim());
+};
+
+/**
+ * Trims the string, adds # if missing and upper or lower cases the string. This
+ * does not mean the string is a valid hex color.
+ * @param {boolean} upper
+ * @returns {string}
+ */
+String.prototype.filterHexColor = function(upper) {
+    var upper = upper || true,
+        code = this.trim();
+    if (code.charAt(0) !== '#') {
+        code = '#' + code;
+    }
+    return !!upper ? code.toUpperCase() : code.toLowerCase();
+};
+
+/**
+ * Escape a string to be used in a regular expression.
+ * @see http://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
+ * @param {string} string
+ * @returns {string}
+ */
+String.prototype.toRegex = function() {
+    return this.replace(regexEscape, "\\$&");
+};
+
+/**
+ * Converts a number to string ordinal number.
+ * @see https://gist.github.com/jlbruno/1535691
+ * @returns {string}
+ */
+Number.prototype.toOrdinal = function() {
+    var s = ["th","st","nd","rd"],
+        v = n % 100;
+    return n + (s[(v - 20) % 10] || s[v] || s[0]);
+};
