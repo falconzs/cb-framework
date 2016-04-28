2142d64f01b57f98014e98037af2417d419c3ecd
falconzs <falconfiftyfive@yahoo.co.uk>
2016-04-28 10:31:02 +0000

    Update various components in index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+require('./prototypes');
 
 exports.app = require('./app');
 exports.bot = require('./bot');
 exports.cbdummy = require('./cb-dummy');
 exports.cbjsdummy = require('./cbjs-dummy');
 exports.collection = require('./collection');
-exports.library = require('./library');
+exports.consts = require('./consts');
 exports.message = require('./message');
 exports.user = require('./user');
 
 exports.pluginAbstract = require('./plugins/abstract');
-exports.pluginHelp = require('./plugins/abstract');
+exports.pluginHelp = require('./plugins/help');
