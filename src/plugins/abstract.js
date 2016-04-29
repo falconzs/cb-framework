f83976ca14794f55d4811bacf5afc9de234f8ea6
falconzs <falconfiftyfive@yahoo.co.uk>
2016-04-29 15:45:41 +0100

    Removed unnecessary satisfies command.

--- src/plugins/abstract.js
+++ src/plugins/abstract.js
@@ -8,19 +8,6 @@ class Abstract {
     setDependencies(api) {
         this.api = api;
     }
-
-    satisfies(command) {
-        if (!this.commands) {
-            return false;
-        }
-        var index, request;
-        for (index in this.commands) {
-            if (index == command) {
-                return this.commands[index];
-            }
-        }
-        return false;
-    }
 }
 
 module.exports = Abstract;
